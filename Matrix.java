b62291aa36f735736e1fa1c096545a0991b179f9
Justin <Justinfigueroa99@icloud.com>
2026-07-25 20:34:15 -0700

    Add program driver

--- Matrix.java
+++ Matrix.java
@@ -48,8 +48,9 @@ public Matrix sub(Matrix matrixB){
     }
     return matrixC;
 }
+//Matrix multiplication class
 public Matrix mult(Matrix matrixB){
-    if (rows != matrixB.rows || cols != matrixB.cols){
+    if (cols != matrixB.rows ){
         throw new IllegalArgumentException("Incompatable array sizes");
     }
     Matrix matrixC = new Matrix(rows, cols);
@@ -63,8 +64,8 @@ public Matrix mult(Matrix matrixB){
         }
     return matrixC;
 }
-
-public void displayOperand(){
+//Mat
+public void displayMatrices(){
     for (int i =0; i < rows; i++){
         for (int j = 0; j < cols; j++){
             System.out.print(data[i][j] + " ");
@@ -115,12 +116,95 @@ private static Matrix fileMatrixGenerator(Scanner fileInput){
 private static int chooseOperation(Scanner userChoice){
     System.out.println("0. Select new matrices");
     System.out.println("1. Addition");
-    System.out.println("1. Subtraction");
-    System.out.println("2. Multiplication");
+    System.out.println("2. Subtraction");
+    System.out.println("3. Multiplication");
     System.out.print("Selection: ");
     return userChoice.nextInt();
 }
 
 
+public static void main(String[] args){
+Scanner userInput = new Scanner(System.in);
+boolean isRunning = true;
+boolean matricesReady = false;
+boolean selectNewMatrices = false;
+ while (isRunning){
+    Matrix matrixA = null;
+    Matrix matrixB = null;
+    Matrix matrixC = null;
+    while (!matricesReady){
+        System.out.println("---------------------");
+        System.out.println("Choose an entry method");
+        System.out.println("---------------------");
+        System.out.println("0.Quit program");
+        System.out.println("1.Manual Entry");
+        System.out.println("2.Read from file");
+        System.out.println("Selection: ");
+        int selection = userInput.nextInt();
+        try {switch (selection) {
+            case 0:
+                isRunning = false;
+                break;
+            case 1:
+            matrixA = manualMatrixGenerator(userInput, "Matrix A");
+            matrixB = manualMatrixGenerator(userInput, "Matrix B");
+            matricesReady = true;
+                break;
+            case 2: 
+            userInput.nextLine();
+            Matrix[] matrices = fileMatrixInitialization(userInput);
+            matrixA = matrices[0];
+            matrixB = matrices[1];
+            matricesReady = true;
+                break;
+            default:
+            System.out.println("Invalid selection. Enter 1 or 2");
+                break;
+        }
+        }
+        catch (IllegalArgumentException error){
+                }
+                matricesReady = true;
+            }
+            System.out.println("Matrix A: ");
+            matrixA.displayMatrices();
+            System.out.println("Matrix B: ");
+            matrixB.displayMatrices();
+            while (isRunning && !selectNewMatrices){
+1
+                int operation = chooseOperation(userInput);
+                try {switch (operation) {
+                    case 0:
+                        selectNewMatrices = true;
+                        break;
+                    case 1: 
+                        matrixC = matrixA.add(matrixB);
+                        System.out.println("Matrix C: ");
+                        matrixC.displayMatrices();
+                            break;
+                    case 2:
+                        matrixC = matrixA.sub(matrixB);
+                        System.out.println("Matrix C: ");
+                        matrixC.displayMatrices();
+                            break;
+                    case 3: 
+                        matrixC = matrixA.mult(matrixB);
+                        System.out.println("Matrix C: ");
+                        matrixC.displayMatrices();
+                            break;
+                    default:
+                        System.out.println("Invalid selection. Enter 0 thorugh 3");
+                        break;
+                    }
+                }
+                catch(IllegalArgumentException error){
+                    
+                }
+            }
+        } 
+        userInput.close();
+        System.out.println("Program Exiting");
+    }
 }
+
 
